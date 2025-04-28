e0c1b6e46fba5cc19f392ab11e3dcd974ccdfee5
koushikbunny <koushikjamboju@gmail.com>
2025-04-29 01:26:58 +0530

    first commit

--- app.py
+++ app.py
@@ -730,5 +730,5 @@ def internal_error(error):
     return render_template('errors/500.html'), 500
 
 if __name__ == '__main__':
-  port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
