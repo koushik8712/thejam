09f836897a6f9f1f07447e84a3da71b3fcaa261f
koushikbunny <koushikjamboju@gmail.com>
2025-04-29 00:02:24 +0530

    first commit

--- database/drop_indexes.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX idx_animal_search ON animals;
-DROP INDEX idx_animal_created ON animals;
--- templates/database/password_resets.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE password_resets (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    token VARCHAR(255) NOT NULL,
-    expires_at DATETIME NOT NULL,
-    used BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
